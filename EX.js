ee8af8453cd393515b4dac4f31982b032f8247e8
Sakshi Pandey <sakshi.pandey0511@gmail.com>
2026-07-17 14:20:34 +0530

    Created EX.js file

---
+++ EX.js
@@ -0,0 +1,14 @@
+//let a=10;
+//const b=25;
+//console.log("a=",a);
+//console.log("b=",b);
+ //b=a++;
+//console.log("b=",b);
+
+
+//function sum(a,b){
+//    return a+b;}
+
+
+const sum=(a,b)=>a+b;
+console.log("Sum",sum(10,90));
